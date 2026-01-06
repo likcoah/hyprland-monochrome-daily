b230d8fe0ae612733fad2a231fe447b222bbb443
likcoah <likcoah@gmail.com>
2026-01-06 20:15:18 +0500

    feat: create neovim config directory and set a base options

---
+++ config/nvim/lua/options.lua
@@ -0,0 +1,33 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.scrolloff = 30
+opt.wrap = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false
+opt.smartindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.clipboard = "unnamedplus"
+opt.mouse = ""
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+
+opt.termguicolors = true
+opt.virtualedit = "block"
