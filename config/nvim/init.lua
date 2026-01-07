d51a4ee0b2f9439db34a2ac5814dfec52f15ea56
likcoah <likcoah@gmail.com>
2026-01-07 16:43:06 +0500

    fix: add keymap.lua to init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,5 @@
 require('config.options')
-
--- require("config.keymaps")
+require('config.keymaps')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
