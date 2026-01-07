cb8350bab90daa3490bf2de4b0812310eb3df8c2
likcoah <likcoah@gmail.com>
2026-01-07 16:03:07 +0500

    feat: add lsp to neovim

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,158 @@
+return {
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.config").setup({
+                ensure_installed = {
+                    "c", "cpp", "go", "lua", "python", "rust", "bash",
+                    "javascript", "typescript", "html", "css", "json", "markdown"
+                },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+    {
+        "williamboman/mason.nvim",
+        dependencies = {
+            "williamboman/mason-lspconfig.nvim",
+        },
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                }
+            })
+        end
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            { "folke/neodev.nvim", opts = {} },
+        },
+        config = function()
+            local lspconfig = require("lspconfig")
+            local mason_lspconfig = require("mason-lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            mason_lspconfig.setup({
+                ensure_installed = {
+                    "clangd",        -- C/C++
+                    "gopls",         -- Go
+                    "pyright",       -- Python
+                    "rust_analyzer", -- Rust
+                    "lua_ls",        -- Lua
+                    "bashls",        -- Bash
+                    "ts_ls",         -- JS/TS
+                    "html",          -- HTML
+                    "cssls",         -- CSS
+                    "jsonls",        -- JSON
+                },
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                    ["lua_ls"] = function()
+                        lspconfig.lua_ls.setup({
+                            capabilities = capabilities,
+                            settings = {
+                                Lua = {
+                                    diagnostics = { globals = { "vim" } },
+                                    workspace = { checkThirdParty = false },
+                                    telemetry = { enable = false },
+                                },
+                            },
+                        })
+                    end,
+                }
+            })
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+        end
+    },
+
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+        config = function()
+            local cmp = require("cmp")
+            local luasnip = require("luasnip")
+
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+			vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#666666", bg = "NONE" })
+			vim.api.nvim_set_hl(0, "FloatBG", { bg = "#202020", fg = "#cccccc" })
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+				window = {
+                    completion = cmp.config.window.bordered({
+						border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+                        winhighlight = "Normal:FloatBG,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+                    }),
+                    documentation = cmp.config.window.bordered({
+						border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+                        winhighlight = "Normal:FloatBG,FloatBorder:Floatborder,CursorLine:PmenuSel,Search:None",
+                    }),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-j>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-k>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+                    ['<C-n>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                    ['<C-b>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                }, {
+                    { name = 'buffer' },
+                    { name = 'path' },
+                })
+            })
+        end
+    }
+}
