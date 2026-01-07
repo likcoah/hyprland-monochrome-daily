937b5f5fb3d5b78661f42cbb3f68623f3b1900bf
likcoah <likcoah@gmail.com>
2026-01-07 15:48:41 +0500

    fix: clean up the telescope and theme plugins spec

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,6 @@
 return {
     'kdheepak/monochrome.nvim',
-    priority = 1000, -- Грузим тему первой
+    priority = 1000,
     config = function()
         vim.cmd("colorscheme monochrome")
     end
